e3a046cdeed6e4156ca0354f0741274b71c36877
iseki <iseki.m.aa@gmail.com>
2014-09-21 08:56:29 +0900

    Make JSMySignUpViewController class.
    * This class is for the customization of sign-up page.

---
+++ jobSquare/jobSquare/JSMySignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  JSMySignUpViewController.h
+//  jobSquare
+//
+//  Created by iseki on 2014/09/21.
+//  Copyright (c) 2014年 Givery. All rights reserved.
+//
+
+#import <Parse/PFSignUpViewController.h>
+
+@interface MySignUpViewController : PFSignUpViewController
+
+@end
