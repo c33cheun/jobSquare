2f8f52522cb7deddf16eb7e0cc428db9636dc9b4
iseki <iseki.m.aa@gmail.com>
2014-09-21 10:21:48 +0900

    Add log out button for development.
    * Red button call logout method.
    * Blue button call loginUserClick.
    * If you want to develop login page, push red button, otherwise push
      blue button.

--- jobSquare/jobSquare/JSLoginViewController.m
+++ jobSquare/jobSquare/JSLoginViewController.m
@@ -18,20 +18,28 @@
 @implementation JSLoginViewController
 
 - (void)loadView{
-    [super loadView];
-    
+	[super loadView];
 }
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-//    UIView *firstView = [[UIView alloc]initWithFrame:CGRectMake(50, 50, self.view.frame.size.width, self.view.frame.size.height)];
-    UIButton *login = [[UIButton alloc]initWithFrame:CGRectMake(100, 500, 100, 50)];
-    [self.view setBackgroundColor:[UIColor whiteColor]];
-    [login setBackgroundColor:[UIColor blueColor]];
-    [login addTarget:self action:@selector(loginUserClick) forControlEvents:UIControlEventAllTouchEvents];
-    //[self.view addSubview:firstView];
-    [self.view addSubview:login];
+	[super viewDidLoad];
+	//	[PFUser logOut];
+	//	[self.navigationController popViewControllerAnimated:YES];
+	// Do any additional setup after loading the view, typically from a nib.
+	//    UIView *firstView = [[UIView alloc]initWithFrame:CGRectMake(50, 50, self.view.frame.size.width, self.view.frame.size.height)];
+	UIButton *login = [[UIButton alloc]initWithFrame:CGRectMake(100, 500, 100, 50)];
+	[self.view setBackgroundColor:[UIColor whiteColor]];
+	[login setBackgroundColor:[UIColor blueColor]];
+	[login addTarget:self action:@selector(loginUserClick) forControlEvents:UIControlEventAllTouchEvents];
+	//[self.view addSubview:firstView];
+	[self.view addSubview:login];
+	
+	UIButton *logout = [[UIButton alloc]initWithFrame:CGRectMake(100, 200, 100, 50)];
+	[self.view setBackgroundColor:[UIColor whiteColor]];
+	[logout setBackgroundColor:[UIColor redColor]];
+	[logout addTarget:self action:@selector(logoutUserClick) forControlEvents:UIControlEventAllTouchEvents];
+	//[self.view addSubview:firstView];
+	[self.view addSubview:logout];
 	
 }
 
@@ -69,40 +77,45 @@
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+- (IBAction)logoutUserClick {
+	[PFUser logOut];
+	[self viewDidAppear:true];
 }
 
 - (IBAction)loginUserClick {
-    JSMapViewController *map;
-    UIViewController *viewController1;
-    UINavigationController *navviewController1 , *navviewController2;
-    
-    viewController1 = [[UIViewController alloc] init];
-    [viewController1 setTitle:@"This is the second view controller"];
-    navviewController1=[[UINavigationController alloc]initWithRootViewController:viewController1];
-    
-    map = [[JSMapViewController alloc] init];
-    navviewController2=[[UINavigationController alloc]initWithRootViewController:map];
-    
-    
-    UITabBarController *tab = [[UITabBarController alloc]init];
-    tab.viewControllers = [NSArray arrayWithObjects: navviewController2, navviewController1, nil];
-    
-    UITabBarItem *mapItem = [[UITabBarItem alloc]init];
-    [mapItem setTitle:@"Map"];
-    //[mapItem setImage:@""];
-
-    UITabBarItem *accountItem = [[UITabBarItem alloc]init];
-    //[accountItem setImage:@""];
-    [accountItem setTitle:@"Account"];
-    
-    [navviewController2 setTabBarItem:mapItem];
-    [navviewController1 setTabBarItem:accountItem];
-    
-    //[self.navigationController pushViewController:tab animated:nil];
-    [self presentViewController:tab animated:YES completion:nil];
-    
+	JSMapViewController *map;
+	UIViewController *viewController1;
+	UINavigationController *navviewController1 , *navviewController2;
+	
+	viewController1 = [[UIViewController alloc] init];
+	[viewController1 setTitle:@"This is the second view controller"];
+	navviewController1=[[UINavigationController alloc]initWithRootViewController:viewController1];
+	
+	map = [[JSMapViewController alloc] init];
+	navviewController2=[[UINavigationController alloc]initWithRootViewController:map];
+	
+	
+	UITabBarController *tab = [[UITabBarController alloc]init];
+	tab.viewControllers = [NSArray arrayWithObjects: navviewController2, navviewController1, nil];
+	
+	UITabBarItem *mapItem = [[UITabBarItem alloc]init];
+	[mapItem setTitle:@"Map"];
+	//[mapItem setImage:@""];
+	
+	UITabBarItem *accountItem = [[UITabBarItem alloc]init];
+	//[accountItem setImage:@""];
+	[accountItem setTitle:@"Account"];
+	
+	[navviewController2 setTabBarItem:mapItem];
+	[navviewController1 setTabBarItem:accountItem];
+	
+	//[self.navigationController pushViewController:tab animated:nil];
+	[self presentViewController:tab animated:YES completion:nil];
+	
 }
 
 @end
